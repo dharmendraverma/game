53159f0ed91ac44b46dd1d844edc2f19d2c319f9
dharmendraSheroes <dharmendra@sheroes.in>
2019-06-01 17:30:25 +0530

    commit

--- app/src/main/java/com/example/dharmendraverma/game/CountDownTimer.java
+++ app/src/main/java/com/example/dharmendraverma/game/CountDownTimer.java
@@ -1,4 +1,17 @@
 package com.example.dharmendraverma.game;
 
-public class CountDownTimer {
+public class CountDownTimer extends android.os.CountDownTimer {
+    public CountDownTimer(long millisInFuture, long countDownInterval) {
+        super(millisInFuture, countDownInterval);
+        // TODO Auto-generated constructor stub
+    }
+
+    @Override
+    public void onFinish() {
+
+    }
+
+    @Override
+    public void onTick(long millisUntilFinished) {
+    }
 }
